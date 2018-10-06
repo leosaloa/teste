bd616d9f0164b0b435a6fff6f951f63e4191e55d
Leonardo Aparecido <leonardosaloa@live.com>
2018-10-06 15:38:23 -0300

    Primeiro Commit

---
+++ aula01.js
@@ -0,0 +1,9 @@
+function teste(){
+	var nome = prompt('Qual é o seu nome? ');
+	var entrada = prompt('Você é Universitário?');
+	if (entrada == 'sim' || entrada == 's' ||  entrada == 'sou') {
+		alert ('Se lasco, '+ nome);  
+	} else {
+    	alert ('OH vida boa em  '+nome+'!'); 
+}
+}
---
+++ teste.js
@@ -0,0 +1,8 @@
+var nomeUsuario = "Eduardo";
+var mensagem = "";
+  
+if (nomeUsuario == "Eduardo") {
+    mensagem = "Nome igual";   
+} else {
+    mensagem = "Nome diferente";   
+}
